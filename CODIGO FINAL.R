c5e47271764ce6e6acb052038aaace3617add230
GarretAlB <145868438+GarretAlB@users.noreply.github.com>
2026-03-04 23:28:17 +0100

    Update CODIGO FINAL.R

--- CODIGO FINAL.R
+++ CODIGO FINAL.R
@@ -21,20 +21,22 @@ columns_to_convert <- c( "date_icu_ad", "start_hs", "end_hs", "na0.1", "na0.2", 
                          "date_lactate_max", "eb10", "eb5", "date_be_max","date_hco3_max", "pf300", "pf200", "pf100", "date_extubation", "date_traqueostomy", 
                          "cr0.3ocrx1.5", "0.5ml/kg/h_en_6-12h", "crx2", "0.5ml/kg/h_en_12h", "crx3o4mg/dl", "0.3ml/kg/h_24h", "date_rrt",  "date_cr_max", "plaq150000", 
                          "plaq100000", "plaq50000", "plaq20000", "bb2", "bb6", "date_drainage", "date_reintervention", "date_icu_discharge", "date_hospital_discharge", "date_death")
+#Fechas a formato legible
 convert_columns_to_date <- function(df, columns) {
   for (col in columns) {
-  df[[col]] <- dmy_hm(df[[col]])#Fechas a formato legible
+  df[[col]] <- dmy_hm(df[[col]])
   }
   return(df)}
 aaa <- convert_columns_to_date(aaa, columns_to_convert)
 dmy(aaa$year_of_birth)->aaa$year_of_birth
-
+dmy_hm(as.character(aaa$reingreso))->aaa$reingreso
+#Comas cambiamos por puntos
 columns_to_convert <- c("ckd","na_dose_pre", "dbt_dose_pre", "mlr_dose_pre", "vsp_dose_pre", "lvef_pre", "na_dose_ad", "dbt_dose_ad", "mlr_dose_ad", "na_dose_6", "dbt_dose_6", 
                         "vsp_dose_6", "lvef_icu", "lvef_ad", "uresis_6", "balance_6","na_max", "dbt_max", "vsp_max", "date_blue_max","troponine_max", "lactate_max", "be_max", 
                         "hco3_max", "cr_max")
 convert_columns_to_numeric <- function(df, columns) {
   for (col in columns) {
-    df[[col]] <- as.numeric(gsub(",", ".", df[[col]]))#Comas cambiamos por puntos
+    df[[col]] <- as.numeric(gsub(",", ".", df[[col]]))
   }
   return(df)}
 aaa <- convert_columns_to_numeric(aaa, columns_to_convert)
@@ -56,7 +58,7 @@ aaa %>%  mutate(imc=weight/((size/100)^2),
                copd=ifelse(is.na(copd),"no_copd",copd),
                osas=ifelse(is.na(osas),"no_osas",osas),
                antithrombotic=ifelse(is.na(antithrombotic),"no",antithrombotic),
-               nyha = ifelse(is.na(nyha),"nyha_1",nyha),  #NYHAS no apuntados son 1s?
+               nyha = ifelse(is.na(nyha),"nyha_1",nyha),  
                age=floor(as.numeric(difftime(aaa$date_icu_ad,aaa$year_of_birth, units = "days"))/365.25),
                sex_factor = ifelse(sex=="hombre", 1, ifelse(sex == "mujer", 0.85, NA)),
                ckd_cockcroft_gault =((140-age)*weight*sex_factor)/(72*ckd),
@@ -72,7 +74,7 @@ aaa %>%  mutate(imc=weight/((size/100)^2),
                vis_6=dbt_dose_6+100*na_dose_6+ifelse(vsp_dose_6==0,0,10000*vsp_dose_6/weight))->aaa
 as.numeric(difftime(aaa$end_hs,aaa$start_hs, units = "min"))->aaa$hs_duration
 
-# Generar variables dummy para cada variable categórica
+#Generar variables dummy para cada variable categórica
 model.matrix(~ hs_type - 1, data = aaa) %>%as.data.frame() %>% select(-hs_typeavr,-hs_typecabg,-hs_typemvr,-hs_typeta)->bbb
 colnames(bbb) <- gsub(" ", "_", colnames(bbb))
 cbind(aaa,bbb)->aaa
@@ -98,7 +100,7 @@ aaa %>%   mutate(
   hs_type_tricuspid_annuloplasty= ifelse(!is.na(hs_type) & hs_type == "tricuspid_annuloplasty" |
                                            !is.na(hs_typeII) & hs_typeII == "tricuspid_annuloplasty" |
                                            !is.na(hs_typeIII) & hs_typeIII == "tricuspid_annuloplasty", "tricuspid_annuloplasty", "no_tricuspid_annuloplasty"))->aaa
-------------------------------
+
 aaa %>% mutate(
   n_major_procedures = rowSums(across(c(hs_type_avr, hs_type_mvr, hs_type_cabg, hs_type_ta, hs_type_tvr, hs_type_tricuspid_annuloplasty),
                                       ~ . == gsub("hs_type_", "", cur_column())), na.rm = TRUE),
@@ -108,7 +110,7 @@ aaa %>% mutate(
     n_major_procedures == 2 ~ "two_major_procedures",
     n_major_procedures >= 3 ~ "three_major_procedures",
     TRUE ~ NA_character_)) -> aaa
-----------------------------------
+
 #Casos con NA en weight_of_procedure
 aaa %>% filter(is.na(weight_of_procedure)) %>% select(nhc, hs_type, hs_typeII, hs_typeIII)
 table(aaa$weight_of_procedure, useNA = "ifany")
@@ -147,8 +149,8 @@ aaa %>% mutate(lvef_change_pre_ad=lvef_pre-lvef_ad,
                pam_ad=(1/3)*pas_ad+(2/3)*pad_ad,
                pam_6=(1/3)*pas_6+(2/3)*pad_6,
                trasfusion=hematíes_post+plaquetas_post+pfc_post)->aaa
-              # indice_shock_modificado_triaje=ifelse(!is.na(tas), fc/(((1/3)*tas)+((2/3)*tad)),NA),
-               #indice_shock_diastolico_triaje=ifelse(!is.na(tad), fc/tad,NA),)
+              #indice_shock_modificado_triaje=ifelse(!is.na(tas), fc/(((1/3)*tas)+((2/3)*tad)),NA),
+              #indice_shock_diastolico_triaje=ifelse(!is.na(tad), fc/tad,NA),)
 aaa %>% select(-na_dose_6,-dbt_dose_6,-vsp_dose_6,-pas_6,-pad_6,-pvc_6,-hr_6,-pm_6,-lvad_6,-uresis_6,-balance_6,-is_6,-pam_6,-...65,-vis_6)->aaa
 
 #r4
@@ -156,19 +158,18 @@ aaa %>% mutate_if(is.character, as.factor)->aaa
 sapply(aaa, class)
 
 #########Calculo del euroescoreII########
-# Definir coeficientes de las variables
+#Definir coeficientes de las variables
 coeficientes <- list(
   age = 0.0285181, sex_female = 0.2196434, insulin_dm = 0.3542749,chronic_pulmonary_dysfunction = 0.1886564, severe_neuromuscular_dysfunction = 0.2407181,
   renal_51_85 = 0.303553, renal_50 = 0.8592256, dialysis = 0.6421508,critical_preop = 1.086517, nyha_II = 0.1070545, nyha_III = 0.2958358, nyha_IV = 0.5597929,
   ccs_class_4 = 0.2226147, extracardiac_arteriopathy = 0.5360268, previous_cardiac_surgery = 1.118599, active_endocarditis = 0.6194522,
   lvef_31_50 = 0.3150652, lvef_21_30 = 0.8084096, lvef_20 = 0.9346919,recent_mi = 0.1528943, pasp_31_54 = 0.1788899, pasp_55 = 0.3491475,
   urgency_urgent = 0.3174673, urgency_emergency = 0.7039121, urgency_salvage = 1.362947,thoracic_aorta_surgery = 0.6527205)
-# Función para calcular la mortalidad predicha
+#Función para calcular la mortalidad predicha
 calcular_mortalidad <- function(euroscoreII) {
   ey <- exp(euroscoreII)
   return(ey / (1 + ey))}
-
-# Calcular EuroSCORE II y mortalidad predicha
+#Calcular EuroSCORE II y mortalidad predicha
 aaa %>%
   mutate(
   euroscoreII = -5.324537 +
@@ -216,13 +217,11 @@ aaa %>%
     if_else(hs_type_ta == "ta", coeficientes$thoracic_aorta_surgery, 0))%>%
   mutate(mortalidad_predicha = calcular_mortalidad(euroscoreII))->aaa
 
-######
+######Cirugias CON bomba
 aaa %>% filter(ecls=="ecls") ->aaa
 
-#muertos en total 15
+#muertos en total
 aaa %>% filter(!is.na(date_death)) %>% select(nhc)
-#muertos con bomba 13
-aaa %>% filter(!is.na(date_death)) %>% filter(ecls=="ecls") %>%  select(nhc)
 
 # Definir la función
 crear_variables_supervivencia_para_todas <- function(data) {
@@ -253,6 +252,31 @@ crear_variables_supervivencia_para_todas <- function(data) {
   return(data)
 }
 aaa1 <- crear_variables_supervivencia_para_todas(aaa)
+aaa1$reingreso
+
+crear_variables_supervivencia_para_todas(aaa1$reingreso)->aaa1$reingreso
+#### Recodificacion
+as.numeric(aaa1$charlson)->aaa1$charlson
+as.numeric(aaa1$cfs)->aaa1$cfs
+as.numeric(aaa1$nyha)->aaa1$nyha
+aaa1 %>% mutate(
+  hf_recat  = factor(ifelse(hf  == "no_hf",  "no_hf",  "yes_hf")),
+  dm_recat  = factor(ifelse(dm  == "no_dm",  "no_dm",  "yes_dm")),
+  smoker_recat = factor(ifelse(smoker == "smoker", "smoker", "no_smoker")),
+  ic_recat  = factor(ifelse(ic  == "no_ic",  "no_ic",  "yes_ic")),
+  lvs_qx_recat = factor(ifelse(lvs_qx == "no_lvs", "no_lvs", "yes_lvs")),
+  time_of_hs_recat = factor(ifelse(time_of_hs == "elective","elective", "urgent_or_emergent")),
+  cardioplegic_sol_recat = factor(case_when(
+    cardioplegic_sol == "sanguinea"                        ~ "sanguinea",
+    cardioplegic_sol %in% c("nido", "buckberg", "saline")  ~ "saline",
+    TRUE                                                   ~ "no_especificada")),
+  cannulation_type_art_recat = factor(case_when(
+    cannulation_type_art == "aortic"                       ~ "central",
+    cannulation_type_art %in% c("axilar", "femoral")       ~ "periferic",
+    TRUE                                                   ~ "other")),
+  lvad_recat = factor(case_when(
+    lvad_pre == "iabp" | lvad_ad == "iabp" ~ "iabp",
+    TRUE                                   ~ "no_iabp"), levels = c("no_iabp","iabp"))) -> aaa1
 
 
 rm(list = c("aaa", "bbb", "r1", "r2", "r3", "r4", "coeficientes","columns_to_convert","pkgs",
